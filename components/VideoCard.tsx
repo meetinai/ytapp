95adc29f8276a09ac1854adc9ccfd4e94811e81d
meetinai <151177530+meetinai@users.noreply.github.com>
2023-12-22 21:36:00 +0530

    Commit from Stub AI (#1)
    Co-authored-by: meetinai <bot@codestub.ai>

---
+++ components/VideoCard.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+interface VideoCardProps {
+  videoData: {
+    "Video ID": string,
+    "Video Title": string,
+    "Video Views": number,
+    "Video Likes": number,
+    "Video Comments": number
+  }
+}
+
+const VideoCard: React.FC<VideoCardProps> = ({ videoData }) => {
+  return (
+    <div className="bg-white shadow-md rounded-lg p-6 m-4">
+      <h2 className="text-2xl font-bold mb-2">{videoData["Video Title"]}</h2>
+      <p><strong>Views:</strong> {videoData["Video Views"]}</p>
+      <p><strong>Likes:</strong> {videoData["Video Likes"]}</p>
+      <p><strong>Comments:</strong> {videoData["Video Comments"]}</p>
+    </div>
+  );
+}
+
+export default VideoCard;
